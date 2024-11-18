1965b7c9b1aeb86956407f2513eaecb56f1fd338
Aaron Sayson <saysonaaronmark@gmail.com>
2024-11-18 22:52:22 +0800

    new font and components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 import classes from "./App.module.css";
 import MainHeader from "./components/MainHeader";
 import SearchBar from "./components/SearchBar";
+import NotesList from "./components/NotesList";
+import WebFont from "webfontloader";
 
 function App() {
+  WebFont.load({
+    google: {
+      families: ['Inter']
+    }
+  });
+
   return (
     <main className={classes.mainContent}>
       <MainHeader />
       <SearchBar />
+      <NotesList />
     </main>
   );
 }
