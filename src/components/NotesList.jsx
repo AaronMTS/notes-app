d35764395b05269dcba87b49c87314c2beee1695
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-08 12:45:45 +0800

    fix: children becoming overstretched if there are a few of them

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -2,12 +2,7 @@ import Note from "./Notes/Note";
 
 const NotesList = () => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4 rounded-lg overflow-y-auto">
-      {/* To be fixed: Note component becomes too stretched when there are only one or two of them inside NotesList.
-        * Potential fix: Count the number of NotesList's children, then apply styling depending on its amount.
-      */}
-      <Note />
-      <Note />
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4 rounded-lg overflow-y-auto">
     </div>
   );
 };
