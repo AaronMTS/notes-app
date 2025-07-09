f7b2e2cd5bcb47abf2cdc17f375bac263ea3e126
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-09 16:14:48 +0800

    feat: dynamic rendering of notes

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -1,12 +1,16 @@
-const Note = () => {
+const Note = ({ content, date }) => {
   return (
-    <div className="flex flex-col text-stone-950 bg-soft-yellow h-56 p-3 rounded-lg">
+    <div className="flex flex-col gap-3.5 text-stone-950 bg-soft-yellow h-56 p-3 rounded-lg">
       <div className="grow overflow-y-auto">
-        <p>Test note</p>
+        <p>{content}</p>
       </div>
+      <hr className="text-yellow-700" />
       <span className="flex justify-between h-6">
-        <p>01/01/2000</p>
-        <button type="button" className="size-6 bg-[url(src/assets/delete.svg)] bg-contain bg-transparent border-none cursor-pointer transition-transform duration-150 active:scale-90"></button>
+        <p>{date}</p>
+        <button
+          type="button"
+          className="size-6 bg-[url(src/assets/delete.svg)] bg-contain bg-transparent border-none cursor-pointer transition-transform duration-150 active:scale-90"
+        ></button>
       </span>
     </div>
   );
---
+++ src/db.js
@@ -0,0 +1,8 @@
+export const NOTES = [
+//   {
+//     id: 1,
+//     details:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+//     date: "January 1, 2000",
+//   },
+];
