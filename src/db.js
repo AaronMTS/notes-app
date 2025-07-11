3ec6a1c32c3fec08dddb5fc8a859dce8f4f695a0
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-11 21:59:58 +0800

    dummy data added

--- src/db.js
+++ src/db.js
@@ -1,8 +1,128 @@
 export const NOTES = [
-//   {
-//     id: 1,
-//     details:
-//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-//     date: "January 1, 2000",
-//   },
+  {
+    id: 1,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 2,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 3,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 4,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 5,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 6,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+  {
+    id: 7,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 8,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 9,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 10,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 11,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 12,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 13,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 14,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 15,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 16,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 17,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
+    {
+    id: 18,
+    title: "Sample",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
+    date: "January 1, 2000",
+  },
 ];
