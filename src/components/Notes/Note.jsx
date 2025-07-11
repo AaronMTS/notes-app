f18cd5783f223f06eea7ff45e81bed35f4a55e88
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-11 22:14:52 +0800

    styling updates and code refactor

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -1,12 +1,15 @@
-const Note = ({ content, date }) => {
+const Note = ({ title, details, date }) => {
   return (
     <div className="flex flex-col gap-3.5 text-stone-950 bg-soft-yellow h-56 p-3 rounded-lg">
-      <div className="grow overflow-y-auto">
-        <p>{content}</p>
+      <div className="grow flex flex-col gap-1">
+        <h6 className="text-lg font-semibold">{title}</h6>
+        <div className="grow overflow-y-auto">
+          <p className="text-sm font-light">{details}</p>
+        </div>
       </div>
       <hr className="text-yellow-700" />
       <span className="flex justify-between h-6">
-        <p>{date}</p>
+        <p className="text-sm font-light">{date}</p>
         <button
           type="button"
           className="size-6 bg-[url(src/assets/delete.svg)] bg-contain bg-transparent border-none cursor-pointer transition-transform duration-150 active:scale-90"
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -9,16 +9,32 @@ const NotesList = () => {
   const noNoteFallback = (
     <div className="absolute top-1/2 left-1/2 -translate-1/2 w-max flex flex-col gap-4 items-center">
       <p className="dark:text-gray-100">There are no notes created yet...</p>
-      <AddNoteButton handleClick={() => navigate("/add")} />
+      <AddNoteButton
+        className="flex gap-1.5 items-center bg-yellow-300 rounded-lg px-2.5 py-2 cursor-pointer hover:bg-yellow-400 active:bg-amber-400 transition-colors"
+        handleClick={() => navigate("/add")}
+      >
+        <span>Add a note</span>
+      </AddNoteButton>
     </div>
-  )
+  );
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4 rounded-lg overflow-y-auto">
-      {NOTES.length > 0 ? NOTES.map(note => {
-        return <Note key={note.id} content={note.details} date={note.date} />
-      }) : noNoteFallback}
-    </div>
+    <>
+      <div className="scrollbar-thin scrollbar-gray-transparent grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4 rounded-lg overflow-y-auto">
+        {NOTES.length > 0
+          ? NOTES.map((note) => {
+              return <Note key={note.id} {...note} />;
+            })
+          : noNoteFallback}
+      </div>
+      {NOTES.length > 0 && (
+        <AddNoteButton
+          className="absolute bottom-8 right-1/2 translate-x-1/2 gap-1 p-3.5 bg-[#1b3d2f] **:fill-white rounded-full custom-shadow cursor-pointer hover:bg-emerald-900 active:bg-emerald-800 md:right-5 md:translate-x-0 md:p-3"
+          handleClick={() => navigate("/add")}
+        >
+        </AddNoteButton>
+      )}
+    </>
   );
 };
 
