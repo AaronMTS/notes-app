98901dd46c389d62f595578099c792485ecf3170
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-11 22:13:55 +0800

    used StateAddNote instead of AddNote

--- src/pages/AddNoteModal.jsx
+++ src/pages/AddNoteModal.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import Modal from "../components/Modal";
-import AddNote from "../components/AddNote";
+// import AddNote from "../components/AddNote";
+import StateAddNote from "../components/StateAddNote";
 
 const AddNoteModal = () => {
   let navigate = useNavigate();
 
   return (
     <Modal handleClick={() => navigate("/")}>
-      <AddNote />
+      <StateAddNote />
     </Modal>
   );
 };
