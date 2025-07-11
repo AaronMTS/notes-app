1b5aed881722244940c48f404bee578ad93309a2
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-11 22:07:04 +0800

    converted to a form

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -15,22 +15,24 @@ const AddNote = () => {
   };
 
   return (
-    <section className="min-w-64 w-[80vw] max-w-md flex flex-col gap-3 bg-slate-100 p-3.5 rounded-lg lg:w-[40vw]">
+    <section className="min-w-64 w-[80vw] max-w-md space-y-3 bg-slate-100 p-3.5 rounded-lg lg:w-[40vw]">
       <header className="flex justify-between items-center">
         <h3 className="text-lg font-semibold">Add a note</h3>
-        <CloseButton handleClick={backToHome} />
+        <CloseButton type="button" handleClick={backToHome} />
       </header>
-      <textarea
-        name=""
-        id=""
-        rows="5"
-        className="outline outline-slate-400 p-2 rounded-lg resize-none font-normal"
-        placeholder="Enter details"
-      ></textarea>
-      <div className="flex justify-end items-center gap-2.5">
-        <CancelButton handleClick={backToHome} />
-        <SaveButton handleSave={saveNote} />
-      </div>
+      <form action="" className="flex flex-col gap-3">
+        <textarea
+          name=""
+          id=""
+          rows="5"
+          className="outline outline-slate-400 p-2 rounded-lg resize-none font-normal"
+          placeholder="Enter details"
+        ></textarea>
+        <div className="flex justify-end items-center gap-2.5">
+          <CancelButton type="reset" handleClick={backToHome} />
+          <SaveButton type="submit" handleSave={saveNote} />
+        </div>
+      </form>
     </section>
   );
 };
