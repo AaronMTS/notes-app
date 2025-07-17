194bb4efb69c1f198950c23a50d255d8dcf8b948
Aaron Sayson <saysonaaronmark@gmail.com>
2025-07-17 20:43:28 +0800

    feat: automated adding of dummy data

--- src/sample-db.js
+++ src/sample-db.js
@@ -2,131 +2,16 @@ export const NEXT_ID = {
   VALUE: 1,
 };
 
-export const NOTES = [
-  {
-    id: 1,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 2,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 3,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 4,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 5,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 6,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-  {
-    id: 7,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 8,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 9,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 10,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 11,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 12,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 13,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 14,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 15,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 16,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 17,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-    {
-    id: 18,
-    title: "Sample",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.",
-    date: "January 1, 2000",
-  },
-];
+const addDummyData = (amount) => {
+  for (let i=0; i<amount; i++) {
+    NOTES.push({
+      id: i,
+      title: `Sample - ${i}`,
+      details: `${i} - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu laudantium aliquip natus voluptatem.`,
+      date: `January ${i + 1}, 2000`
+    })
+  }
+}
+
+export const NOTES = [];
+addDummyData(10)
