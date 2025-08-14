0212a431b3ee201bbe766795e25429a2a785b718
Aaron Sayson <saysonaaronmark@gmail.com>
2025-08-14 15:52:34 +0800

    feat: added click handler to `SaveButton`; allowed `Cancel Button` to have dynamic content

--- src/components/Buttons/CancelButton.jsx
+++ src/components/Buttons/CancelButton.jsx
@@ -1,11 +1,11 @@
-const CancelButton = ({ handleClick, ...props }) => {
+const CancelButton = ({ handleClick, content="Cancel" , ...props }) => {
   return (
     <button
       {...props}
       onClick={handleClick}
       className="outline outline-red-300 px-3 py-1.5 rounded-md cursor-pointer hover:bg-red-100 active:bg-red-200 transition-colors"
     >
-      Cancel
+      {content}
     </button>
   );
 };
--- src/components/Buttons/SaveButton.jsx
+++ src/components/Buttons/SaveButton.jsx
@@ -1,6 +1,7 @@
-const SaveButton = ({ ...props }) => {
+const SaveButton = ({handleClick, ...props }) => {
   return (
     <button
+      onClick={handleClick}
       {...props}
       className="bg-yellow-300 px-3.5 py-1.5 rounded-md cursor-pointer hover:bg-yellow-400 active:bg-amber-400 transition-colors"
     >
